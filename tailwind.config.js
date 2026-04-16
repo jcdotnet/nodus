7206031aed3b1b35b671cddb731321d6b4ba5846
JC <jcdotnet@hotmail.com>
2026-04-16 10:21:14 +0200

    chore: initial scaffold with angular 18 and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: '#0f172a',    // Slate 900
+          primary: '#3b82f6', // Blue 500
+        }
+      }
+    },
+  },
+  plugins: [],
+}
